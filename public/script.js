f1eb5e4542aa90d72b73f1886e9ede33eb0b6621
ArmanVD <Arman22.av@gmail.com>
2025-03-19 11:12:44 +0100

    import gsap

--- public/script.js
+++ public/script.js
@@ -1,3 +1,5 @@
+import gsap from "gsap";
+
 document.querySelectorAll("a[class^='card-']").forEach((card) => {
   card.addEventListener("mouseenter", () => {
     const newColor = getComputedStyle(card)
